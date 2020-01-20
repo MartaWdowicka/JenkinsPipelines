426954ccd1cf6cea4f1b8edb02d3279fc59879bc
Mwdowicka <mwdowicka@kamsoft.pl>
2020-01-20 10:29:02 +0100

    s

--- src/example/junit/TestCalculator.java
+++ src/example/junit/TestCalculator.java
@@ -7,7 +7,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
 
+ 
 public class TestCalculator {
  
 	private math.Calculator calc;
@@ -25,25 +27,25 @@ public class TestCalculator {
 	 @Test
 	 public void testAdd() {
 		  double result = calc.add(10,20);
-		  org.junit.Assert.assertEquals(30, result, 0);
+		  assertEquals(30, result, 0);
 	 }
 	 
 	 @Test
 	 public void testSubtract() {
 		  double result = calc.subtract(100,50);
-		  org.junit.Assert.assertEquals(50, result, 0);
+		  assertEquals(50, result, 0);
 	 }
 	 
 	 @Test
 	 public void testMultiply() {
 		  double result = calc.multiply(2.5, 100);
-		  org.junit.Assert.assertEquals(250, result, 0);
+		  assertEquals(250, result, 0);
 	 }
 	 
 	 @Test
 	 public void testDivide() {
 		  double result = calc.divide(100, 10);
-		  org.junit.Assert.assertEquals(10, result, 0.001);
+		  assertEquals(2, result, 0.001);
 	 }
 	  
 	 @Test(expected = ArithmeticException.class)
